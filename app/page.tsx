f27ae47e7537a4770af6ebe7b11beb985f09bed6
Su Hyun Jung <junction@uw.edu>
2026-05-10 00:57:41 -0700

    Adjust layout positioning and dimensions in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,16 +58,16 @@ export default function IntroPage() {
           opacity-50
           mix-blend-color-dodge
       
-          w-[330vw]
-          left-[calc(50%+110vw)]
-          top-[8vh]
+          w-[335vw]
+          left-[calc(50%+100vw)]
+          top-[7vh]
       
           md:w-[260vw]
-          md:left-[calc(50%+90vw)]
+          md:left-[calc(50%+75vw)]
           md:top-[1vh]
       
           xl:w-[180vw]
-          xl:left-[calc(50%+20vw)]
+          xl:left-[calc(50%+30vw)]
           xl:top-[0vh]
       
           2xl:w-[150vw]
